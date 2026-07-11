60ec4057775451bb4920c6c4bc93932a9bef4b03
Palak Singh <palak27055@gmail.com>
2026-07-11 19:04:25 +0530

    introduction to datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello");  we are using nodejs , not browsers
+
+// console.log(4 + 
+//     5);                         // code readibility matters..don't write codes like this.
+//     console.log("palak")
+
+
+let name = "palak"
+let age = 20
+let isLoggeedIn = true
+let state
+let stage = null
+
+// primitive dataTypes------->
+
+// number--> 2 to power 53
+// bigInt --> for bigger numbers
+//string --> use in double quotes
+// boolean --> true/ false
+// null --> standAlone value
+// undefined --> not declared intentionally
+// symbol --> unique
+
+// objects --> non primitive dataTypes
+
+console.log(typeof age)  //number
+console.log(typeof name) //string
+console.log(typeof isLoggeedIn) //boolean
+console.log(typeof stage) //object
+console.log(typeof state) //undefined
