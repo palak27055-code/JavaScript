cf0df48ab2653310f46523c3ac5af3b4709e8dd7
Palak Singh <palak27055@gmail.com>
2026-07-14 20:38:36 +0530

    logical operators and assignment operators

--- 01_Basics/05_logical.js
+++ 01_Basics/05_logical.js
@@ -13,6 +13,19 @@ console.log(! isGoogleUser);
 
 
 
+// ******************************* ASSIGNMENT OPERATORS *****************************
+
+let num1 = 10;
+// num1 += 5
+// num1 *= 5
+// num1 /= 5
+// num1 -= 5
+
+console.log(num1);
+
+
+
+
 
 
 
