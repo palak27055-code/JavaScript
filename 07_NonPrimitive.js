7daee5fb3af075bd528a7ff46c671ed526855703
Palak Singh <palak27055@gmail.com>
2026-07-16 18:30:45 +0530

    non primitive

---
+++ 07_NonPrimitive.js
@@ -0,0 +1,59 @@
+// Object---->
+
+// const username = {
+//     firstname: "palak", 
+//     isLoggedin: true
+// };
+
+// username.firstname = "shivam";
+// username.lastname = "Singh";
+
+// console.log(username.firstname);
+// console.log(username);
+// console.log(typeof username);  // you can extra data as well..
+
+
+// another way to access the object data(with spaces between name of variale)----->
+
+// const user = {
+//     "first name": "Palak",
+//     "last name": "Singh",
+//     isLoggedin: true
+// };
+
+// console.log(user["first name"]);
+// console.log(user["last name"]);
+
+
+
+// Date as a object---->
+
+let today = new Date();
+// console.log(today.getMinutes());
+// console.log(today.getDate());
+// console.log(today.getHours());
+
+
+
+// Array---->
+
+let heroes = ["Ironman", "Spiderman", "Thor", "Hulk", true, 34, null];
+// console.log(heroes);
+// console.log(heroes[0]);
+
+
+
+
+let isValue = true;
+console.log((isValue) + 2);
+console.log((isValue));
+console.log(Number(isValue));
+
+
+
+
+
+
+
+
+
