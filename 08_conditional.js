a2db67edcb2f38bc5e8e663948c5a6b59309f21e
Palak Singh <palak27055@gmail.com>
2026-07-18 18:14:15 +0530

    ...

---
+++ 08_conditional.js
@@ -0,0 +1,62 @@
+// check if a number is greater than other number:----->
+
+// let a  = 10;
+// let b  = 10;
+// if(a>b){
+//     console.log("a is greater"); 
+// }
+// else if(b>a){
+//     console.log("b is greater");   
+// }
+// else{
+//     console.log("both are same"); 
+// }
+
+
+
+// checking if a string is equal to another string:---->
+
+// let string1 = "palak"
+// let string2 = "Palak"
+// if(string1 == string2){
+//     console.log("yes it is same");
+// }
+// else{
+//     console.log("no it's not");
+// }    
+
+
+
+//check if a variable is a number or not:---->
+
+// let score = 90;
+
+// if (typeof score == "number"){
+//     console.log("yes it's a number");
+// }else{
+//     console.log("nope");
+// }
+
+
+
+//check if a boolean value is true or false: --->
+
+// let isLoggedin = false;
+// if(isLoggedin){
+//     console.log("it's true");
+// }else{
+//     console.log("nope");  
+// }
+
+
+
+//check if arrray is empty or not:----->
+
+// let arr = [1,2,3,4,5];
+// if(arr.length == 0){
+//     console.log("it's empty");
+// }else{
+//     console.log("nope");
+    
+// }
+
