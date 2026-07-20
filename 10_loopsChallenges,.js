3517bcaa7de95a97c01a7aff473ff7c15449bc68
Palak Singh <palak27055@gmail.com>
2026-07-20 12:06:49 +0530

    ...

---
+++ 10_loopsChallenges,.js
@@ -0,0 +1,21 @@
+// 1. write a while loop that calculates the sum of all  numbers from 1 to 5 and prints the result to the console in varaible 'sum.
+
+// let i = 1;
+// var sum = 0;
+// while (i <= 5) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// 2. write a 'while' loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown'.
+let i = 5;
+let countdown = [];
+while (i >= 1) {
+    (countdown.push(i));
+    i--;
+}
+console.log(countdown);
+
+
