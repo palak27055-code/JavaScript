ad72e3e03e49834e8d32a9fb26de4d8c36366548
Palak Singh <palak27055@gmail.com>
2026-07-21 19:35:20 +0530

    challenges on loops

---
+++ 10_loopsChallenges.js
@@ -0,0 +1,75 @@
+// 1. write a while loop that calculates the sum of all  numbers from 1 to 5 and prints the result to the console in varaible 'sum.
+
+// let i = 1;
+// var sum = 0;
+// while (i <= 5) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// 2. write a 'while' loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown'.
+
+// let i = 5;
+// let countdown = [];
+// while (i >= 1) {
+//     (countdown.push(i));
+//     i--;
+// }
+// console.log(countdown);
+
+
+
+// 3. write a 'do while ' loop that prompts  a user to enter their favorite tea type until they enter "stop". 
+//   store each tea type in a array named 'teaCollectioon ' .
+
+// let teaCollection = [];
+// let tea
+
+// do {
+//     tea = prompt("enter your favorite tea (STOP to stop): ");
+//         teaCollection.push(tea);
+     
+// }
+// while (tea != "stop")
+// console.log(teaCollection);
+
+
+// 4. write a do while loop that adds numbers from 1 to 3 and stores the result in a var named 'total'
+
+// let total = 0;
+// let i = 1;
+// do{
+//    total = total + i;
+//    i++;
+// }while(i<=3)
+    
+// console.log(total);
+
+
+
+// 5. write a for loop that multiplies each element in the array [2,4,6] and stores the result in a new named array 'multipliedNumbers'
+
+// let arr = [2, 4, 6];
+// let product = 1;
+
+// for(let i = 0; i<arr.length; i++){
+//     product *= arr[i];
+// }
+// let multipliedNumbers = product;
+// console.log(multipliedNumbers);
+
+
+// 6. write a for loop that lists all the cities in the array ["paris", "new york", "tokyo", "london"] and stores each city in a new array named 'cityList'.
+
+// let cities = ["paris", "new york", "tokyo", "london"]
+// let cityList = [];
+// for(let i = 0; i<cities.length; i++){
+//     cityList.push(cities[i]);
+// }
+// console.log(cityList);
+
+
+
+
