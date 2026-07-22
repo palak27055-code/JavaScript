abf944da171b13b6b309ea286d0c7eaa1a7211af
Palak Singh <palak27055@gmail.com>
2026-07-22 20:59:19 +0530

    .

--- 11_levelUpChallenges.js
+++ 11_levelUpChallenges.js
@@ -42,6 +42,16 @@
  
 // 4. Use a `for-of` loop to iterate through the array `["chai", "green tea", "herbal tea", "black tea"]` and skip `"herbal tea"`.Store the other teas in an array named `preferredTeas`.
 
+// let teas =["chai", "green tea", "herbal tea", "black tea"];
+// let preferredTeas = [];
+// for (const tea of teas) {
+//     if(tea == "herbal tea"){
+//         continue;
+//     }
+//     preferredTeas.push(tea)
+// }
+// console.log(preferredTeas);
+
 
 
 
