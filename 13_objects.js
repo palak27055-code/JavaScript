d78fec304b31a7ec6822c51e47e13247a3ad80c5
Palak Singh <palak27055@gmail.com>
2026-07-23 21:10:25 +0530

    objects in javaScript

--- 13_objects.js
+++ 13_objects.js
@@ -1,5 +1,4 @@
 // object literals--->
-
 const mySym = Symbol("key1");
 
 const user = {
@@ -12,12 +11,22 @@ const user = {
     [mySym]: "mykey1"          //symbol as key
 };
 
-console.log(user.email);   // less used way to access value of key
-console.log(user["age"]);
-console.log(user["full name"]);
-console.log(user[mySym]);
+// console.log(user.email);   // less used way to access value of key
+// console.log(user["age"]);
+// console.log(user["full name"]);
+// console.log(user[mySym]);
 
 
 user.email = "shiv@example.com";  //updating value of key
-Object.freeze(user);              //freezing the object so that no changes can be made to it
-console.log(user.email);
+// Object.freeze(user);              //freezing the object so that no changes can be made to it
+// console.log(user.email);
+
+
+// adding method---->
+
+user.greet = function() {   //adding a method to the object
+   console.log("Hello user");
+}
+
+// console.log(user.greet());
+
