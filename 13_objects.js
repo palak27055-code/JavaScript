f687749f9b0d13acc4acef305e98f5dc2c7be9f1
Palak Singh <palak27055@gmail.com>
2026-07-23 18:12:53 +0530

    .

---
+++ 13_objects.js
@@ -0,0 +1,23 @@
+// object literals--->
+
+const mySym = Symbol("key1");
+
+const user = {
+    name: "palak",  //key-value pair
+    "full name": "palak singh",  //spaces in key
+    age: 20,
+    isStudent: true,
+    location: "India",
+    email: "palak@example.com",
+    [mySym]: "mykey1"          //symbol as key
+};
+
+console.log(user.email);   // less used way to access value of key
+console.log(user["age"]);
+console.log(user["full name"]);
+console.log(user[mySym]);
+
+
+user.email = "shiv@example.com";  //updating value of key
+Object.freeze(user);              //freezing the object so that no changes can be made to it
+console.log(user.email);
