2fbfb890f12ea0ea3244fad7074a63bbb67d5fb2
Palak Singh <palak27055@gmail.com>
2026-07-24 18:43:42 +0530

    .

--- 14_objects-2.js
+++ 14_objects-2.js
@@ -29,8 +29,24 @@ const obj3 = {5: "a", 6: "b"}
 
 // const obj3 = {obj1 , obj2}
 // const obj4 = Object.assign( obj1 , obj2, obj3)    // obj1 is target here
-const obj4 = Object.assign({}, obj1 , obj2, obj3)  // empty array is target here
-console.log(obj4);
+// const obj4 = Object.assign({}, obj1 , obj2, obj3)  // empty array is target here
+
+const obj4 = {...obj1 , ...obj2, ...obj3}  // spread operator
+// console.log(obj4);
+
+
+// database--->
+
+const users = [
+    { id: 1, email: 'user1@example.com', name: 'User 1' },
+    { id: 2, email: 'user2@example.com', name: 'User 2' },
+    { id: 3, email: 'user3@example.com', name: 'User 3' }
+]
+
+
+
+
+
 
 
 
