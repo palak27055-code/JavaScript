2d2374a2b253c9f7922291b3bbcc9bc5f9fa63c5
Palak Singh <palak27055@gmail.com>
2026-07-24 18:32:44 +0530

    Objects in javaScript

---
+++ 14_objects-2.js
@@ -0,0 +1,37 @@
+// const tinderUser = new Object();    // object constructor syntax
+const tinderUser = {};                 // object literal syntax or singleton syntax
+
+tinderUser.id = '123abc';
+tinderUser.name = 'John';
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+
+// nesting in objects--->
+
+const regularUser = {
+    email: 'regular@example.com',
+    fullName : {
+        username : "palak",
+        lastname : "singh"
+    }
+}
+// console.log(regularUser["fullName"]["username"]);
+
+// console.log(regularUser.fullName.username);
+// console.log(regularUser.fullName?.username);  // ? is used if you are not sure if any key(fullName here) is available her or not.
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj4 = Object.assign( obj1 , obj2, obj3)    // obj1 is target here
+const obj4 = Object.assign({}, obj1 , obj2, obj3)  // empty array is target here
+console.log(obj4);
+
+
+
+
