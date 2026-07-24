59f441dc08688108090c46736e9c6e35ffac8365
Palak Singh <palak27055@gmail.com>
2026-07-24 19:11:32 +0530

    objects in javaScrip

--- 14_objects-2.js
+++ 14_objects-2.js
@@ -46,6 +46,17 @@ const users = [
 
 
 
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("ia"));  // false 
+
+
+
+
 
 
 
