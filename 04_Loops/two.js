bf5575827755ad724f5d4456aeab1f4d16a54ab8
Palak Singh <palak27055@gmail.com>
2026-08-30 14:43:45 +0530

    while

---
+++ 04_Loops/two.js
@@ -0,0 +1,36 @@
+// ==========================
+// while loop
+// ==========================
+// let a = 8
+// i = 0
+// while (i<a) {
+    // console.log(i);
+//     i++
+// }
+
+// i = 0
+// while (i<=10) {
+    // console.log(i);
+    // i+=5
+// }
+
+// ==========================
+// while loop in array
+// ==========================
+
+// let arr = ["aa", "bb", "cc", "dd"]
+// let i = 0
+// while (i<arr.length) {
+//     console.log(arr[i] + ` ${i}`);
+//     i++
+// }
+
+// ==========================
+// do while loop 
+// ==========================
+
+let score =1
+do{
+    console.log(score);
+    score++
+}while(score>=10)
