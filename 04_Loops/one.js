abeba3b893d316b6690e9481ee98b0e29c25fc90
Palak Singh <palak27055@gmail.com>
2026-08-30 13:33:02 +0530

    .

--- 04_Loops/one.js
+++ 04_Loops/one.js
@@ -19,14 +19,19 @@ for (let i = 0; i <= 10; i++) {
 //============================
 // loop in loop
 //=============================
-for (let i = 0; i < 10; i++) {
-    console.log(`Outer loop value: ${i}`);
-    for (let j = 0; j < 2; j++) {
+for (let i = 1; i < 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j < 10; j++) {
         // console.log(`Inner loop value: ${j} and ${i}`);
-        console.log(i+`*` + j + `=` + i*j);
-        
-        
+        // console.log(i+`*` + j + `=` + i*j);    
     }
-    
-    
+}
+
+// ============================
+// array in loops 
+// ============================
+let myArray = ["flash", "superman", "batman", "wonder woman",]
+for(let index = 0; index < myArray.length; index++) {
+    const element = myArray[index]
+    // console.log(element);
 }
