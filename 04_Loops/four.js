caf6c897105d23d7ecf51f669fd8447633580dc2
Palak Singh <palak27055@gmail.com>
2026-09-06 15:18:14 +0530

    four

--- 04_Loops/four.js
+++ 04_Loops/four.js
@@ -7,3 +7,20 @@ const myObject = {
 for (const key in myObject) {
     console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United states of America")
+map.set('UAE', "United Arab")
+map.set('IN', "India")
+
+for (const key in map) {
+   console.log(key);
+
+}
